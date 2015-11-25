70e13ba62da5352dfc9fae8589958ce003c8091c
Tony Nguyen <dolongky@gmail.com>
2015-11-25 11:22:17 +0700

    Refactor middle

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
-    logger: function (req, res, next) {
-        var date = new Date().toString();
-        console.log('Request on ' + date + ': ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
