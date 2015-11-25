20cc15e1e49783554addf407246c6d6207c7d150
Tony Nguyen <dolongky@gmail.com>
2015-11-25 10:56:24 +0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('private route hit!');
+        next();
+    },
+    logger: function (req, res, next) {
+        var date = new Date().toString();
+        console.log('Request on ' + date + ': ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', function (req, res) {
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+    console.log('Express server started at port ' + port + '!');
+});
